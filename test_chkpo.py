db0598934cefb018a51f4cfd5d8a4c993481f90c
Eleni Tapta <tapta.eleni@gmail.com>
2023-03-11 16:05:04 +0200

    Delete test_chkpo.py

--- test_chkpo.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.training import checkpoint_utils as cp
-
-print(cp.list_variables('.\\VAE\\results_gumbel_softmax\\checkpoint\\run9(5modes)\\trpo_plugins\\decoder\\decoder_model_e2000.ckpt'))
-print(cp.list_variables('.\\VAE\\results_gumbel_softmax\\checkpoint\\run9(5modes)\\trpo_plugins\\encoder\\encoder_model_e2000.ckpt'))
-
